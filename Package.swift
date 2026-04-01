de754b842191ad61917dcc07cbf24136eb215b09
vongvoro <vongvoro@daou.co.kr>
2026-04-01 16:13:37 +0900

    feat: Package Name 수정

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "KMPModule",
+    name: "baseStarter",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         .library(
-            name: "KMPModule",
+            name: "baseStarter",
             targets: ["KMPModule"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "KMPModule",
+            name: "baseStarter",
             url: "https://github.com/DaouMobile/DOP-iOS-KMP-XCFramework/releases/download/v1.0.0/KMP_1.0.0.zip",
             checksum: "1e1f7c12b7aa7fac07ff3fc18f0fc7eb0d358e70c540153cfe882153ea80e00a"
         )
