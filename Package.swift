e2f85064cad6c51c76bd17f96cee39566a9f6c83
daoutech-vongvoro <143379352+daoutech-vongvoro@users.noreply.github.com>
2026-04-01 10:49:39 +0900

    Update Package.swift for 1.0.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KMPModule",
-            url: "https://github.com/DaouMobile/DOP-iOS-KMP-XCFramework/releases/download/v1.0.2/KMP_1.0.2.zip",
+            url: "https://github.com/DaouMobile/DOP-iOS-KMP-XCFramework/releases/download/v1.0.3/KMP_1.0.3.zip",
             checksum: "1e1f7c12b7aa7fac07ff3fc18f0fc7eb0d358e70c540153cfe882153ea80e00a"
         )
     ]
