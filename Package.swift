11e20e77c0e94f60f7bd278479be1dd495d41e95
vongvoro <vongvoro@daou.co.kr>
2026-04-03 15:40:16 +0900

    feat: baseStarter target 이름 변경

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "baseStarter",
-            targets: ["KMPModule"]
+            targets: ["baseStarter"]
         )
     ],
     targets: [
