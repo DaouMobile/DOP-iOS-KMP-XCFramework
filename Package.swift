8223945a9fbddd65c803f4e615efa955fe2d91a7
daoutech-nahanwoong <nahanwoong@daou.co.kr>
2026-06-25 16:54:09 +0900

    Update Package.swift for 1.0.55

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "baseStarter",
-            url: "https://github.com/DaouMobile/DOP-iOS-KMP-XCFramework/releases/download/v1.0.54/KMP_1.0.54.zip",
-            checksum: "06ede9aec5d06829f1d89e9f8782fe1b0b820c78b01fb189d6b46c3223682305"
+            url: "https://github.com/DaouMobile/DOP-iOS-KMP-XCFramework/releases/download/v1.0.55/KMP_1.0.55.zip",
+            checksum: "f669858f4c9e113f8e0738c7b0a7d9dbb9bdeb28a870f39d0d277c7eb2252c21"
         )
     ]
 )
